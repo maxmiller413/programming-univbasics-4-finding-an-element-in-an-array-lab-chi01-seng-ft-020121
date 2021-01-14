c73793a2059dbe512fc2891e794e64318b0cf727
Max Miller <maxmiller413@gmail.com>
2021-01-14 18:41:00 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -1,6 +1,6 @@
 def find_element_index(array, value_to_find)
   count = 0 
-  while count < array.length do 
+  array.length.times |i| do 
     if array[count] == value_to_find
       return count 
     end 
